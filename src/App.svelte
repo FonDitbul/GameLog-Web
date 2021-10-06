1f2a512a271a3b481abd4484bce236cbf31c6bfa
FonDitbul <niki7084@naver.com>
2021-10-06 23:47:15 +0900

    svelete test

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	export let name: string;
+	import Home from './home.svelte';
 </script>
 
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {name}!</h1>	
+	<Home num={42}/>
 </main>
+
+
 
 <style>
 	main {
---
+++ src/home.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    // export const tempNumber1: number = 123;
+    // export const tempNumber2: number = 456;
+    // export const tempNumber3: number = 789;
+    export let num: number;
+</script>
+
+
+<ul>
+    
+    <l1>{num}</l1>
+    <li>{num}</li>
+    <li>{num}</li>
+</ul>
