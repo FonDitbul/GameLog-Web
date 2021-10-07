ac2e1c6b0d9df145e1152698d23e3560fa2c2244
FonDitbul <niki7084@naver.com>
2021-10-07 15:09:43 +0900

    input test

--- src/route/Search.svelte
+++ src/route/Search.svelte
@@ -1,1 +1,17 @@
-<h1>search</h1>
+<script>
+    let text = ''
+</script>
+
+
+<h1>
+    {text}
+</h1>
+<input type="text"
+        value={text}
+        on:input={(e) => {text = e.target.value}} />
+<input type="text"
+        bind:value={text} />
+
+<button on:click={() => {text = 'Click event'}}>
+    Click
+</button>
