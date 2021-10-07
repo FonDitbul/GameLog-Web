e6b7c38fd32b3d69a7acda39c7b0b7cb972ac478
FonDitbul <niki7084@naver.com>
2021-10-07 14:48:00 +0900

    Navbar 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	export let name: string;
 	import Home from './navbarHome.svelte';
+	let toggle = false;
 </script>
+
+<ul class="navbar">
+	<l1><img src="//public/gamelog_favicon.png"></l1>
+	<li><a href="/"> Home | </a></li>
+	<li><a href="/Library"> Library | </a></li>
+	<li><a href="/wishlist"> Wishlist | </a></li>
+	<li><a href="/Search"> Serach | </a></li>
+
+	<li><a href="/login"> login | </a></li>
+	<li><a href="/signup"> register </a></li>
+</ul>
+
+<button on:click={()=> {toggle = !toggle}}>
+	Toggle
+</button>
+{#if toggle}
+	<h2> hi toggle! </h2>
+{:else}
+	<h2> no toggle </h2>
+{/if}
 
 
 <main>
@@ -30,5 +51,16 @@
 		main {
 			max-width: none;
 		}
+	}
+	ul.navbar{
+		list-style:none;
+		margin:10;
+		padding:0;
+	}
+	li{
+		margin: 10;
+		padding: 10;
+		border : 10;
+		float:left;
 	}
 </style>
