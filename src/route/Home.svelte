8809478252dad5aa2737cb2d1dca1ab85a72e772
FonDitbul <niki7084@naver.com>
2021-10-09 16:54:36 +0900

    폴더 변경

---
+++ src/route/Home.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import HomeGameCategory from "./homeGameCategory.svelte"
+    import testGame from "../test/game.json"
+    
+    const categories = testGame.map((i)=>{
+        const gameName = i.game.map((j)=>{return j.name})
+        return i.type
+    })
+    
+    
+</script>
+
+
+<div>
+    <!-- {#each category as categories} -->
+    <HomeGameCategory category={categories[0]}/>
+    <HomeGameCategory category={categories[1]}/>
+    <HomeGameCategory category={categories[2]}/>
+    <HomeGameCategory category={categories[3]}/>
+    <HomeGameCategory category={categories[4]}/>
+    <HomeGameCategory category={categories[5]}/>
+    <!-- {/each} -->
+</div>
---
+++ src/route/homeGameCategory.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import testGame from "../test/game.json";
+    export let category:string;
+    let gameList = [];
+    testGame.map((i)=>{
+        if(i.type === category){
+            i.game.map((j)=>{
+                gameList.push(j)
+            })
+        }
+    })
+    console.log(gameList)
+    import Image from './imagetest.svelte';
+
+</script>
+
+
+<div>
+    {category}
+    <ul>
+        {#each gameList as game}
+            <li>
+                <p>{game.name}</p>
+                <Image src={game.cover}/>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+
+</style>
