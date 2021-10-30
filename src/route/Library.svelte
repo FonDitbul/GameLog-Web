f72305dba7743f8de9e4452708812ad5f2957205
FonDitbul <niki7084@naver.com>
2021-10-30 17:55:28 +0900

    library, wishlist 게임 생성 태그

--- src/route/Library.svelte
+++ src/route/Library.svelte
@@ -6,6 +6,8 @@
 		{name:'first_relase_date', 'text':'출시일 순'},
 		{name:'createdTime', 'text':'담은 날짜 순'},
 	]
+	import testGame from "../test/game.json"
+    let tempLibgame = testGame[0]
 </script>
 
 <div class="library">
@@ -16,6 +18,25 @@
             </option>
         {/each}
     </select>
-
-
+    <ul>
+        {#each tempLibgame.game as Game}
+            <div class="static">
+                <li class="box">
+                    <img src={Game.cover[0].url}>
+                    <p>{Game.name}</p>
+                </li>
+            </div>
+        {/each}
+    </ul>
 </div>
+
+<style>
+    .box {
+        width: 300px;
+        border: 1px solid #aaa;
+        border-radius: 2px;
+        box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+        padding: 1em;
+        margin: 0 0 1em 0;
+    }
+</style>
--- src/route/Wishlist.svelte
+++ src/route/Wishlist.svelte
@@ -1,5 +1,42 @@
-<script>
-    import {storeName} from './store'; //store test code
+<script lang="ts">
+	let selected:string;
+	let options = [
+		{name:'aggregated_rating', 'text':'기관 점수 순'},
+		{name:'name', 'text':'이름순'},
+		{name:'first_relase_date', 'text':'출시일 순'},
+		{name:'createdTime', 'text':'담은 날짜 순'},
+	]
+	import testGame from "../test/game.json"
+	let tempLibgame = testGame[0]
 </script>
 
-<h1>{$storeName}</h1>
+<div class="library">
+    <select bind:value={selected}>
+        {#each options as option}
+            <option value={option}>
+                {option.text}
+            </option>
+        {/each}
+    </select>
+    <ul>
+        {#each tempLibgame.game as Game}
+            <div class="static">
+                <li class="box">
+                    <img src={Game.cover[0].url}>
+                    <p>{Game.name}</p>
+                </li>
+            </div>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    .box {
+        width: 300px;
+        border: 1px solid #aaa;
+        border-radius: 2px;
+        box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+        padding: 1em;
+        margin: 0 0 1em 0;
+    }
+</style>
