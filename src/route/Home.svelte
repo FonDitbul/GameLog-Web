893e23a9e2cdf1195eacd56492c7622ef87a474c
FonDitbul <niki7084@naver.com>
2021-11-01 01:17:59 +0900

    Home 서버에서 데이터 받아오기

--- src/route/Home.svelte
+++ src/route/Home.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
-    import HomeGameCategory from "./homeGameCategory.svelte"
-    import testGame from "../test/HomeGame.json"
+	import HomeGameCategory from "./homeGameCategory.svelte"
+	import testGame from "../test/HomeGame.json"
+	import {serverURL} from "../envStore";
+
+	let promise;
+
+	async function getServer(){
+		const response = await fetch('http://localhost:3000/')
+		// console.log(response.json())
+		return new Promise((resolve, reject) => {
+			response.json()
+				.then(data=>{
+					resolve(data.data)
+				})
+		})
+	}
+
+	import {beforeUpdate, onMount} from 'svelte'
+	beforeUpdate(async()=>{ //HTML이 mount 된후에 작동하는 code
+		promise = getServer();
+	})
+
 </script>
 
-
 <div>
-     {#each testGame as category}
-        <HomeGameCategory category="{category}"/>
-     {/each}
+    {#await promise}
+    {:then Games}
+        {#each Games as category}
+            <HomeGameCategory category="{category}"/>
+        {/each}
+    {:catch error}
+        <p>{error}</p>
+    {/await}
 </div>
