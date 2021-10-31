70c642865ff23d92ff493227ff52f8b3f9d770cf
FonDitbul <niki7084@naver.com>
2021-11-01 00:46:10 +0900

    게임 detail 변경

--- src/route/GameDetail.svelte
+++ src/route/GameDetail.svelte
@@ -2,40 +2,40 @@
 	import { game } from "../test/gameDetail"
 </script>
 
+<h3>
+    게임 detail
+</h3>
+
 <div class="game">
     <div>
-        <h1>게임이름 : {game.gameDetail.name}</h1>
-        <h1>game.cover 커버 사진</h1>
-        <h1>기관 평점 : {game.gameDetail.aggregated_rating}</h1>
-        <h1>기관 평가 수 : {game.gameDetail.aggregated_rating_count}</h1>
+        <h3>게임이름 : {game.gameDetail.name}</h3>
+        <h3>game.cover 커버 사진</h3>
+        <h3>기관 평점 : {game.gameDetail.aggregated_rating}</h3>
+        <h3>기관 평가 수 : {game.gameDetail.aggregated_rating_count}</h3>
 
-        <h1>게임 카테고리 : {game.gameDetail.category}</h1>
-        <h1>게임 장르 : {game.gameDetail.genres[0].name}</h1>
-        <h1>게임 테마 : {game.gameDetail.themes[0].name}</h1>
-        <h1>게임 플랫폼 : {game.gameDetail.platforms[0].name}</h1>
+        <h3>게임 카테고리 : {game.gameDetail.category}</h3>
+        <h3>게임 장르 : {game.gameDetail.genres[0].name}</h3>
+        <h3>게임 테마 : {game.gameDetail.themes[0].name}</h3>
+        <h3>게임 플랫폼 : {game.gameDetail.platforms[0].name}</h3>
 
 
-        <h1>출시일 : {game.gameDetail.first_release_date}</h1>
-        <h1>연관 기업 : {game.gameDetail.involved_companies[0].company[0].name}</h1>
+        <h3>출시일 : {game.gameDetail.first_release_date}</h3>
+        <h3>연관 기업 : {game.gameDetail.involved_companies[0].company[0].name}</h3>
 
-        <h1>game.screenshots 인게임 샷</h1>
-        <h1>게임 스토리 : {game.gameDetail.storyline}</h1>
-        <h1>게임 요약 : {game.gameDetail.summary}</h1>
+        <h3>game.screenshots 인게임 샷</h3>
+        <h3>게임 스토리 : {game.gameDetail.storyline}</h3>
+        <h3>게임 요약 : {game.gameDetail.summary}</h3>
     </div>
     <hr>
     <div>
-        <h1>유저 평점 : {game.userGame.userGameRating}</h1>
-        <h1>유저 리뷰 : {game.userGame.userGameMemo}</h1>
-        <h1>상태값 : {game.userGame.userGameStatus}</h1>
+        <h3>유저 평점 : {game.userGame.userGameRating}</h3>
+        <h3>유저 리뷰 : {game.userGame.userGameMemo}</h3>
+        <h3>상태값 : {game.userGame.userGameStatus}</h3>
     </div>
 </div>
 
 
 <style>
-    h1{
-        margin:5px;
-        width:300px;
-    }
     .game{
         float: left;
     }
