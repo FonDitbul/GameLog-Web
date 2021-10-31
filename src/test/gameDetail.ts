d0b077c7731b6ceba4705d13ded0d551ac94af06
FonDitbul <niki7084@naver.com>
2021-10-31 10:52:53 +0900

    Game detail example 데이터 생성

---
+++ src/test/gameDetail.ts
@@ -0,0 +1,221 @@
+export let game:any =
+	{
+		"gameDetail": {
+			"id" : 12517,
+			"name" : "Undertale",
+			"slug" : "undertale",
+			"aggregated_rating" : 4.82,
+			"aggregated_rating_count" : 16,
+			"alternative_names" : [
+				{
+					"id" : 5928,
+					"name" : "アンダーテール",
+				},
+				{
+					"id" : 5929,
+					"name" : "UT",
+				},
+				{
+					"id" : 50179,
+					"name" : "언더테일",
+				}
+			],
+			"involved_companies" : [
+				{
+					"id" : 116640,
+					"company" : [
+						{
+							"id" : 6545,
+							"name" : "tobyfox",
+						}
+					],
+				},
+				{
+					"id" : 116641,
+					"company" : [
+						{
+							"id" : 23459,
+							"name" : "temmie",
+						}
+					],
+				},
+				{
+					"id" : 134860,
+					"company" : [
+						{
+							"id" : 29538,
+							"name" : "8-4",
+						}
+					],
+				}
+			],
+			"first_release_date" : 1442275200,
+			"created_at" : 1442097206,
+			"updated_at" : 1633301566,
+			"release_dates" : [
+				45798,
+				45799,
+				53824,
+				97199,
+				97200,
+				155763,
+				157718,
+				157719,
+				233933
+			],
+			"category" : "main_game",
+			"game_modes" : [
+				{
+					"id" : 1,
+					"name" : "Single player",
+				}
+			],
+			"genres" : [
+				{
+					"id" : 12,
+					"name" : "Role-playing (RPG)",
+				},
+				{
+					"id" : 16,
+					"name" : "Turn-based strategy (TBS)",
+				},
+				{
+					"id" : 31,
+					"name" : "Adventure",
+				},
+				{
+					"id" : 32,
+					"name" : "Indie",
+				}
+			],
+			"platforms" : [
+				{
+					"id" : 3,
+					"category" : 4,
+					"name" : "Linux",
+					"category_name" : "operating_system"
+				},
+				{
+					"id" : 6,
+					"category" : 4,
+					"name" : "PC (Microsoft Windows)",
+					"category_name" : "operating_system"
+				},
+				{
+					"id" : 14,
+					"category" : 4,
+					"name" : "Mac",
+					"category_name" : "operating_system"
+				},
+				{
+					"id" : 46,
+					"category" : 5,
+					"name" : "PlayStation Vita",
+					"category_name" : "portable_console"
+				},
+				{
+					"id" : 48,
+					"category" : 1,
+					"name" : "PlayStation 4",
+					"category_name" : "console"
+				},
+				{
+					"id" : 49,
+					"category" : 1,
+					"name" : "Xbox One",
+					"category_name" : "console"
+				},
+				{
+					"id" : 130,
+					"category" : 1,
+					"name" : "Nintendo Switch",
+					"category_name" : "console"
+				}
+			],
+			"themes" : [
+				{
+					"id" : 17,
+					"name" : "Fantasy",
+				},
+				{
+					"id" : 27,
+					"name" : "Comedy",
+				},
+				{
+					"id" : 31,
+					"name" : "Drama",
+				}
+			],
+			"storyline" : "\"A long time ago, two races ruled peacefully over the Earth: HUMANS and MONSTERS. One day, a terrible war broke out between the two races. After a long battle, the humans were victorious. They sealed the monsters underground with a magical spell. \n \nIn the year 201X, a small child scales Mt. Ebott. It is said that those who climb the mountain never return. \n \nSeeking refuge from the rainy weather, the child enters a cave and discovers an enormous hole. \n \nMoving closer to get a better look... the child falls in. \n \nNow, our story begins.\"",
+			"summary" : "A small child falls into the Underground, where monsters have long been banished by humans and are hunting every human that they find. The player controls the child as they try to make it back to the Surface through hostile environments, all the while engaging with a turn-based combat system with puzzle-solving and bullet hell elements, as well as other unconventional game mechanics.",
+			"cover" : [
+				{
+					"id" : 103865,
+					"game" : 12517,
+					"height" : 800,
+					"width" : 600,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/co2855.jpg",
+				}
+			],
+			"screenshots" : [
+				{
+					"id" : 20147,
+					"game" : 12517,
+					"height" : 476,
+					"width" : 634,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/tz5ypjqhppmtypognq7o.jpg",
+				},
+				{
+					"id" : 20148,
+					"game" : 12517,
+					"height" : 480,
+					"width" : 640,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/ze8i79ycm3gyymrjvxf0.jpg",
+				},
+				{
+					"id" : 20149,
+					"game" : 12517,
+					"height" : 480,
+					"width" : 640,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/dp7ytj3ouesyoq7ddrrx.jpg",
+				},
+				{
+					"id" : 20150,
+					"game" : 12517,
+					"height" : 480,
+					"width" : 640,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/njxoybaxsgaxso8qadmo.jpg",
+				},
+				{
+					"id" : 20151,
+					"game" : 12517,
+					"height" : 480,
+					"width" : 640,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/thidwzj4rqnuurycg3dy.jpg",
+				}
+			],
+			"__v" : 0
+		},
+		"userGame": {
+			"userid": 2,
+			"gameId": 12517,
+			"gameName": "Undertale",
+			"cover" : [
+				{
+					"id" : 103865,
+					"game" : 12517,
+					"height" : 800,
+					"width" : 600,
+					"url" : "//images.igdb.com/igdb/image/upload/t_thumb/co2855.jpg"
+				}
+			],
+			"aggregated_rating" : 4.82,
+			"aggregated_rating_count" : 16,
+			"first_release_date" : 1442275200,
+			"userGameRating": 4,
+			"userGameMemo":"유저 리뷰 와! 언더테일!!",
+			"userGameStatus": "doing",
+			"createdTime": 1635639472,
+			"id": 2841
+		}
+	}
