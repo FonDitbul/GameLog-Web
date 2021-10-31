b57b387d3eabf1accfa72e0dc9f998e590980ded
FonDitbul <niki7084@naver.com>
2021-11-01 00:47:30 +0900

    search 탭 변경

--- src/route/Search.svelte
+++ src/route/Search.svelte
@@ -2,18 +2,28 @@
     let text = ''
 </script>
 
-<div class="center">
-    <h1>
-        {text}
-    </h1>
+<h1>게임 검색</h1>
+<div class="search">
     <input type="text"
             value={text}
             on:input={(e) => {text = e.target.value}} />
-    <input type="text"
-            bind:value={text} />
     
-    <button on:click={() => {text = 'Click event'}}>
-        Click
+    <button on:click={() => {console.log('search', text)}}>
+        검색
     </button>
 </div>
 
+<style>
+    .search{
+        /*display: flex;*/
+        margin:auto;
+        /*float: left;*/
+    }
+    input{
+        width:500px;
+        margin: 30px 10px 20px 10px;
+    }
+    button{
+        margin: 10px;
+    }
+</style>
