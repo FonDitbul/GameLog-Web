e9b78409f7784d1230547fe817684a39bc63cb8a
FonDitbul <niki7084@naver.com>
2021-11-01 13:08:14 +0900

    store 관리 추가

---
+++ src/envStore.ts
@@ -0,0 +1,5 @@
+import {writable} from 'svelte/store'
+
+// export let serverURL = writable('http://localhost:3000/');
+
+export const API_URL:string = 'http://localhost:3000/'
--- src/route/store.ts
+++ src/route/store.ts
@@ -1,3 +1,10 @@
 import {writable} from 'svelte/store'
 
 export let storeName:any = writable('GameLog');
+export let gameDetail:Object = writable({
+
+})
+
+export let user:Object = writable({
+
+})
