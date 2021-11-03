139060d90dc23b70c42e3ef04d5c27c15469b191
FonDitbul <niki7084@naver.com>
2021-11-04 00:46:21 +0900

    임시 배포

--- src/route/Home.svelte
+++ src/route/Home.svelte
@@ -5,23 +5,23 @@
 	import { API_URL } from "../envStore";
 
 	let promise;
-	async function getServer(){
-		const response = await fetch(API_URL,{
-			method:'GET',
-            // body:{temp:'test'}
-        })
-		// console.log(response.json())
-		return new Promise((resolve, reject) => {
-			response.json()
-				.then(data=>{
-					resolve(data.data)
-				})
-		})
-	}
+	// async function getServer(){
+	// 	const response = await fetch(API_URL,{
+	// 		method:'GET',
+    //         // body:{temp:'test'}
+    //     })
+	// 	// console.log(response.json())
+	// 	return new Promise((resolve, reject) => {
+	// 		response.json()
+	// 			.then(data=>{
+	// 				resolve(data.data)
+	// 			})
+	// 	})
+	// }
 
 	import {beforeUpdate, onMount} from 'svelte'
 	beforeUpdate(async()=>{ //HTML이 mount 된후에 작동하는 code
-		promise = getServer();
+		// promise = getServer();
 	})
 
 </script>
