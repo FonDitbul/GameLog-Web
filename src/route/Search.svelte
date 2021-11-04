3866f5706832341b8535f6da4151009cd3232bfc
FonDitbul <niki7084@naver.com>
2021-11-04 21:01:43 +0900

    search 구현 서버연동

--- src/route/Search.svelte
+++ src/route/Search.svelte
@@ -1,16 +1,48 @@
 <script>
-    let text = ''
+    import {API_URL} from "../envStore";
+    let searchText = ''
+    async function searchGame (gameName) {
+        const searchURL = `?name=${gameName}`;
+        const response = await fetch(API_URL + 'search' + searchURL, {
+            method: 'GET',
+            headers: {
+                // 'Accept': 'application/json',
+                'Content-Type': "application/json",
+            },
+        })
+        return response.json();
+    }
+    const onKeyPress = e => {
+        if (e.charCode === 13) {
+            searchGame(searchText)
+                .then(res=>{
+                    console.log(res)
+                })
+        }
+    }
 </script>
 
 <h1>게임 검색</h1>
 <div class="search">
     <input type="text"
-            value={text}
-            on:input={(e) => {text = e.target.value}} />
-    
-    <button on:click={() => {console.log('search', text)}}>
-        검색
-    </button>
+           value={searchText}
+           on:keypress={onKeyPress}
+           on:input={(e) => {
+                searchText = e.target.value
+            searchGame(e.target.value)
+        .then(res=>{
+            console.log(res)
+        })
+            }} />
+
+<!--    <button on:click={() => {-->
+<!--        searchGame(searchText)-->
+<!--        .then(res=>{-->
+<!--            console.log(res)-->
+<!--        })-->
+<!--    }}>-->
+<!--        검색-->
+<!--    </button>-->
 </div>
 
 <style>
