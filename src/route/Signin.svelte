c6b6f7af3533be279b1c338c40d9247350ad7b2c
FonDitbul <niki7084@naver.com>
2021-11-04 20:33:36 +0900

    로그인 서버 연동 구현

--- src/route/Signin.svelte
+++ src/route/Signin.svelte
@@ -1,22 +1,53 @@
 <script lang="ts">
-    let text:string = '';
+    import { API_URL } from "../envStore";
+
+	let signinEmail: string = '';
+	let signinPassword: string = '';
+	async function signInSubmit(email, password) {
+		const data = {
+			email:email,
+			password:password
+		}
+		const response = await fetch(API_URL + 'auth/login', {
+			method: 'POST',
+			headers: {
+				// 'Accept': 'application/json',
+				'Content-Type': "application/json",
+			},
+			body: JSON.stringify(
+				data
+			),
+		})
+        return response.json();
+	}
+
 </script>
 
 <div class="center">
     <h4> 로그인 </h4>
     <div>
         <input type="text"
-        value={text}
-        on:input={(e) => {text = e.target.value}} 
-        placeholder="email"/>
+            value={signinEmail}
+            on:input={(e) => {signinEmail = e.target.value}}
+            placeholder="email"/>
     </div>
     
     <div>
         <input type="password"
-        placeholder="password"/>
+               value={signinPassword}
+               on:input={(e) => {signinPassword = e.target.value}}
+               placeholder="password"/>
     </div>
     
-    <button on:click={() => {}}>
+    <button on:click={() => {
+        signInSubmit(signinEmail, signinPassword)
+        .then(res=>{
+            if(res.user){
+                alert(res.message)
+                window.location.href='/'
+            }
+        });
+    }}>
         로그인
     </button>
     <button on:click={() => {}}>
