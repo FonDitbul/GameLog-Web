d34cd95ba997daae7f32e101799256611340578e
FonDitbul <niki7084@naver.com>
2021-11-05 10:23:40 +0900

    wishlist get 구현

--- src/route/Wishlist.svelte
+++ src/route/Wishlist.svelte
@@ -9,7 +9,29 @@
 		{name:'createdTime', 'text':'담은 날짜 순'},
 	]
 	import testGame from "../test/HomeGame.json"
+	import {API_URL} from "../envStore";
+	import {beforeUpdate} from "svelte";
 	let tempLibgame = testGame[0]
+
+	async function getServer(){
+		const response = await fetch(API_URL + 'game/wishlist',{
+			method:'GET',
+			credentials: "include",
+		})
+		return new Promise((resolve, reject) => {
+			response.json()
+				.then(data=>{
+					console.log(data)
+					resolve(data.data)
+				})
+		})
+	}
+	let wishlistPromise;
+	beforeUpdate(async()=>{ //HTML이 mount 된후에 작동하는 code
+		wishlistPromise = getServer();
+		// console.log(libraryPromise);
+		// libraryPromise.then(r=>{console.log(r)})
+	})
 </script>
 
 <h1>
@@ -25,16 +47,21 @@
         {/each}
     </select>
     <table class="library-table">
-        {#each tempLibgame.game as Game}
-            <td>
-                <Link to="/gamedetail?id={Game.id}">
-                    <div class="box">
-                        <img src={Game.cover[0].url}>
-                        <p>{Game.name}</p>
-                    </div>
-                </Link>
-            </td>
-        {/each}
+        {#await wishlistPromise}
+        {:then wishlistGame}
+            {#each wishlistGame as Game}
+                <td>
+                    <Link to="/gamedetail?id={Game.gameId}">
+                        <div class="box">
+                            <img src={Game.cover.url}>
+                            <p>{Game.gameName}</p>
+                        </div>
+                    </Link>
+                </td>
+            {/each}
+        {:catch error}
+            <h1>{error}</h1>
+        {/await}
     </table>
 </div>
 
