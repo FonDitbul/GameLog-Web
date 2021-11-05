1d51839faaafe0c44e86b3d7218a5f5ae8d7a5a8
FonDitbul <niki7084@naver.com>
2021-11-05 10:16:31 +0900

    Home server 연동 구현

--- src/route/Home.svelte
+++ src/route/Home.svelte
@@ -5,34 +5,33 @@
 	import { API_URL } from "../envStore";
 
 	let promise;
-	// async function getServer(){
-	// 	const response = await fetch(API_URL,{
-	// 		method:'GET',
-    //         // body:{temp:'test'}
-    //     })
-	// 	// console.log(response.json())
-	// 	return new Promise((resolve, reject) => {
-	// 		response.json()
-	// 			.then(data=>{
-	// 				resolve(data.data)
-	// 			})
-	// 	})
-	// }
+	async function getServer(){
+		const response = await fetch(API_URL,{
+			method:'GET',
+			credentials: "include",
+        })
+		return new Promise((resolve, reject) => {
+			response.json()
+				.then(data=>{
+					resolve(data.data)
+				})
+		})
+	}
 
 	import {beforeUpdate, onMount} from 'svelte'
 	beforeUpdate(async()=>{ //HTML이 mount 된후에 작동하는 code
-		// promise = getServer();
+		promise = getServer();
 	})
 
 </script>
 
 <div>
-    <!--{#await promise}-->
-    <!--{:then Games}-->
-        {#each testGame as category}
+    {#await promise}
+    {:then Games}
+        {#each Games as category}
             <HomeGameCategory category="{category}"/>
         {/each}
-    <!--{:catch error}-->
-    <!--    <p>{error}</p>-->
-    <!--{/await}-->
+    {:catch error}
+        <p>{error}</p>
+    {/await}
 </div>
